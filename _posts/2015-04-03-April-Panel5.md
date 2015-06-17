4a06a01d6d8fdceeddd9811e5852ed68fd3de97f
Aaron Pedersen <aaron@developmentarc.com>
2015-06-16 22:19:29 -0700

    Updates to layout

--- _posts/2015-04-03-April-Panel5.md
+++
@@ -1,8 +0,0 @@
----
-layout: post
-title: Map
-published: true
----
-
-<!-- {{ page.title }} Section -->
-<div id="map"></div>
--- js/rockon.js
+++ js/rockon.js
@@ -18,7 +18,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: ($($anchor.attr('href')).offset().top)
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
@@ -38,17 +38,17 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 12,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(33.7683, -118.1956), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map. 
+        // How you would like to style the map.
         // This is where you would paste any style found on Snazzy Maps.
         styles: [{
             "featureType": "water",
@@ -160,19 +160,25 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map 
+    // Get the HTML DOM element that will contain your map
     // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
+    var points = [];
+    points.push( {lat: 33.769582, lng: -118.189811} ); // we labs
+    points.push( {lat: 33.827577, lng: -118.147347} ); // gunn jerkins
+
+    _.each(points, function(point) {
+        var image = 'img/map-marker.png';
+        var myLatLng = new google.maps.LatLng(point.lat, point.lng);
+        var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
         icon: image
     });
+    })
+    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
 }
